60730e19b57ac474e7b9a703f2350e85656834d2
Krittin Tragunejindarat <itin752@gmail.com>
2023-08-15 21:57:12 +0700

    add checkbox (sync color)

--- src/components/todobox/todobox.jsx
+++ src/components/todobox/todobox.jsx
@@ -1,17 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './todobox.css'
 
 function TodoBox({ prop }) {
-    let bgColor;
-    if (prop.completed == 0) {
-        bgColor = '#F7F48B';
-    } else if (prop.completed == 1) {
-        bgColor = '#A2EF44';
+    const [isChecked, setIsChecked] = useState(prop.completed);
+    const [bgColor, setBgColor] = useState(prop.completed ? '#A2EF44' : '#F7F48B');
+    const handleCheckboxChange = () => {
+        const newIsChecked = !isChecked;
+        setIsChecked(!isChecked);
+        setBgColor(newIsChecked ? '#A2EF44' : '#F7F48B');
     }
     return (
         <>
             <div className="box-container" style={{ backgroundColor: bgColor }}>
-                <div className="title">{prop.title}</div>
+                <input
+                    type="checkbox"
+                    checked={isChecked}
+                    onChange={handleCheckboxChange}
+                />
+                <div className="text">{prop.title}</div>
             </div>
         </>
     )
